5d14681e8f0f9c3d8cf647d08eff779a4f0ea17a
Dieter De Meyer <dieterdemeyer@gmail.com>
2014-01-04 20:46:27 +0100

    Redesign of sudo configuration module

---
+++ manifests/config/cmnd_alias.pp
@@ -0,0 +1,66 @@
+# Define: sudo::config::cmnd_alias
+#
+# This manages sudo Cmnd_Alias configurations
+#
+# Parameters:
+# [*ensure*]
+# Ensure if present or absent.
+# Default: present
+#
+# [*configuration*]
+# Content of configuration snippet.
+# Default: undef
+#
+# Actions:
+# Installs sudo Cmnd_Alias configuration
+#
+# Requires:
+# Class sudo
+# stdlib
+#
+# Sample Usage:
+# sudo::config::cmnd_alias { 'admins-elev' :
+#   configuration => 'ADMINELEV = ALL'
+# }
+#
+# [Remember: No empty lines between comments and class definition]
+define sudo::config::cmnd_alias(
+  $ensure='present',
+  $configuration=undef
+) {
+
+  validate_re($title, '^[A-Za-z0-9_\-.]+$', "Sudo::Config::Cmnd_alias['${title}']: title value does not match ^[A-Za-z0-9_.]+$")
+
+  #Class['sudo'] -> Sudo::Config::Cmnd_alias[$title]
+
+  case $ensure {
+    'absent': {
+      include sudo::params
+
+      file { "sudo/config/cmnd_alias/${title}":
+        ensure => $ensure,
+        path   => "${sudo::params::config_dir}/cmnd_aliases/${title}"
+      }
+    }
+    'present': {
+      if $configuration {
+        include sudo::params
+
+        file { "sudo/config/cmnd_alias/${title}":
+          ensure  => $ensure,
+          owner   => 'root',
+          group   => 'root',
+          mode    => '0440',
+          path    => "${sudo::params::config_dir}/cmnd_aliases/${title}",
+          content => template('sudo/config/cmnd_alias.erb')
+        }
+      } else {
+        fail("Sudo::Config::Cmnd_alias['${title}']: required parameter configuration not specified")
+      }
+    }
+    default: {
+      fail("Sudo::Config::Cmnd_alias['${title}']: parameter ensure must be present or absent")
+    }
+  }
+
+}
---
+++ spec/defines/config/host_alias_spec.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/env rspec
+
+require 'spec_helper'
+
+describe 'sudo::config::host_alias' do
+  context 'with title foo' do
+    let (:title) { 'foo' }
+    #let (:pre_condition) { "class sudo {}" }
+
+    context 'on osfamily RedHat' do
+      let (:facts) { {
+        :osfamily => 'RedHat'
+      } }
+
+      context 'with default parameters' do
+        let (:params) { { } }
+
+        it { expect { subject }.to raise_error(
+            Puppet::Error, /required parameter configuration not specified/
+        )}
+      end
+
+      context 'with configuration => test' do
+        let (:params) { { :configuration => 'test' } }
+
+        it { should include_class('sudo::params') }
+
+        it { should contain_file('sudo/config/host_alias/foo').with_owner('root') }
+        it { should contain_file('sudo/config/host_alias/foo').with_group('root') }
+        it { should contain_file('sudo/config/host_alias/foo').with_mode('0440') }
+        it { should contain_file('sudo/config/host_alias/foo').with_path('/etc/sudoers.d/host_aliases/foo') }
+        it { should contain_file('sudo/config/host_alias/foo').with_content(/Host_Alias    test/) }
+      end
+    end
+  end
+
+  context 'with title foo bar' do
+    let (:title) { 'foo bar' }
+
+    context 'on osfamily RedHat' do
+      let (:facts) { {
+        :osfamily => 'RedHat'
+      } }
+
+
+      context 'with configuration => test' do
+        let (:params) { { :configuration => 'test' } }
+
+        it { expect { subject }.to raise_error(
+            Puppet::Error, /title value does not match/
+        )}
+      end
+    end
+  end
+end
